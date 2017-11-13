fb448ac42c1f9570dfacefbbed24bc7fbc9d79c8
gpm17 <gpm17@gpm17vm>
2017-11-13 19:26:25 +0100

    soap-tut finished

---
+++ GPM17/src/Approve.java
@@ -0,0 +1,14 @@
+
+public class Approve {
+
+	public boolean isApproved(long studenId) {
+		return true;
+	}
+	
+	
+//	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//
+//	}
+
+}
